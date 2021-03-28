d506f47a7d2d55b8f857b24b74d639c46820e1a4
daniel.roek <daniel.roek@gmail.com>
2021-03-28 17:06:49 +0200

    Added item stuff

--- lib/home/item_list_view.dart
+++ lib/home/item_list_view.dart
@@ -14,25 +14,53 @@ class _ItemListViewState extends State<ItemListView> {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<DataBloc, DataState>(
-        bloc: BlocProvider.of<DataBloc>(context),
-        builder: (context, state) {
-          if (state is HasDataState) {
-            return ListView(
-              children: _buildItems(state),
-            );
-          } else {
-            return Container();
-          }
-        },
+      bloc: BlocProvider.of<DataBloc>(context),
+      builder: (context, state) {
+        if (state is HasDataState) {
+          return ListView(
+            children: _buildItems(state),
+          );
+        } else {
+          return Container();
+        }
+      },
     );
   }
 
   List<Widget> _buildItems(HasDataState state) {
     return List<Widget>.of(() sync* {
       for (final FlipItem item in state.items) {
-        debugPrint('got one');
         yield Card(
-          child: ListTile(title: Text(item.name),),
+          child: ListTile(
+            trailing: IconButton(icon: Icon(Icons.bookmark), onPressed:  () {
+              //TODO: Bookmark item
+            },),
+            leading: Icon(Icons.image),
+            title: Text(item.name),
+            subtitle: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('sell: ${item.high!}'),
+                    Text('buy: ${item.low!}'),
+                    Text('buyVolume: ..')
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('limit: ${item.buyLimit!}'),
+                    Text('roi: ${item.roi! * 100}%'),
+                    Text('sellVolume: ..'),
+
+                  ],
+                )
+              ],
+            ),
+          ),
         );
       }
     }());
