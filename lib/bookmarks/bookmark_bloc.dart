19874e38ffbc5d89023dcf2a386c852b016f5445
daniel.roek <daniel.roek@gmail.com>
2021-03-28 20:39:56 +0200

    Added logic in bookmark_bloc.dart

--- lib/bookmarks/bookmark_bloc.dart
+++ lib/bookmarks/bookmark_bloc.dart
@@ -2,18 +2,38 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:osrs_flipper/data_bloc/model/flip_item.dart';
 
 part 'bookmark_event.dart';
 part 'bookmark_state.dart';
 
 class BookmarkBloc extends Bloc<BookmarkEvent, BookmarkState> {
-  BookmarkBloc() : super(BookmarkInitial());
+  List<FlipItem> bookmarks= [];
+  String bookmarkKey = 'bookmarkKey';
+  final _store = GetStorage();
+  BookmarkBloc() : super(BookmarkInitial()) {
+    bookmarks = _loadBookmarks();
+  }
 
   @override
   Stream<BookmarkState> mapEventToState(
     BookmarkEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+  if (event is AddBookmark) {
+   yield* _handleAddBookmark(event);
+  }
+  }
+
+  Stream<BookmarkState> _handleAddBookmark(AddBookmark event) async* {
+    bookmarks.add(event.savedItem);
+    await _store.write(bookmarkKey, bookmarks);
+    yield BookmarkListState(bookmarks);
+  }
+
+  List<FlipItem> _loadBookmarks() {
+    bookmarks = _store.read(bookmarkKey) ?? [];
+    return bookmarks;
   }
 }
