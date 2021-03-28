b5b9ab99ff0f62829201d71c4e41a31fb6909130
daniel.roek <daniel.roek@gmail.com>
2021-03-28 16:32:15 +0200

    Added Item

--- lib/data_bloc/model/flip_item.dart
+++ lib/data_bloc/model/flip_item.dart
@@ -1,9 +1,10 @@
 class FlipItem {
+  final int id;
   final double roi;
   final int low;
   final int high;
   final int buyLimit;
   final String name;
 
-  const FlipItem(this.roi, this.low, this.high, this.buyLimit, this.name);
+  const FlipItem(this.id, this.roi, this.low, this.high, this.buyLimit, this.name);
 }
